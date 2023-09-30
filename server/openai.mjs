bab132db3f5905f37504d5e0c76b671b8da3f2c4
KhromeM <68210337+KhromeM@users.noreply.github.com>
2023-09-30 16:38:22 -0400

    lots of stuff

--- server/openai.mjs
+++ server/openai.mjs
@@ -10,9 +10,12 @@ export async function esgSummary(companyName) {
 			{
 				role: "system",
 				content:
-					'You are an impersonal assistant that gives a concise professonial summary of a public company\'s ESG profile. Users will tell you a compnay name and you will respond with the summary. If you do not know enough, respond with "Unknown" DO NOT EVER reveal you are a chatbot. NEVER refer to yourself. DO NOT include any disclaimers or say the information is not up to date. Keep answers to a maximum of 100 words.',
+					'You are a summary generator program that gives a concise summary of a public company\'s ESG profile. Users will tell you a compnay name and you will respond with the summary. If you do not know enough, respond with "Unknown" DO NOT EVER reveal you are a chatbot. NEVER refer to yourself. DO NOT include any disclaimers or say the information is not up to date or that you do not have enough information. Keep answers to a maximum of 100 words.',
 			},
-			{ role: "user", content: companyName },
+			{
+				role: "user",
+				content: `Write me an objective paragraph about the company : ${companyName} \nAnswer whether this company positively or negatively addresses ESG concerns`,
+			},
 		],
 	});
 	const message = res.choices[0].message.content;
--- server/server.mjs
+++ server/server.mjs
@@ -1,11 +1,16 @@
 import fs, { writeFileSync } from "fs";
-import express from "express";
+import express, { json } from "express";
 const app = express();
 import { esgSummary } from "./openai.mjs";
+import cors from "cors";
 
-const bin = fs.readFileSync("./data.json");
+app.use(cors());
+app.use(json());
+
+const bin = fs.readFileSync("./data_final.json");
+const { averageBySector, sectors, tickers } = JSON.parse(bin);
+
 const bin2 = fs.readFileSync("./summaries.json");
-const { keys, averageBySector, sectors, tickers } = JSON.parse(bin);
 const summaries = JSON.parse(bin2);
 
 // const summaries = {};
@@ -14,7 +19,7 @@ const summaries = JSON.parse(bin2);
 // 		const companyName = tickers[ticker];
 // 		const m = await esgSummary(ticker);
 // 		summaries[ticker] = m;
-// 		console.log(summaries);
+// 		console.log(m + "\n\n");
 // 	}
 // 	writeFileSync("./summaries.json", JSON.stringify(summaries));
 // })();
@@ -26,54 +31,86 @@ app.get("/esg/:ticker", (req, res) => {
 	console.log(`Got request! Ticker: ${ticker}`);
 	const obj = tickers[ticker];
 	if (obj) {
-		const [T, E, G, S, C] = [
-			obj["Total"],
-			obj["Environment Risk Score"],
-			obj["Governance Risk Score"],
-			obj["Social Risk Score"],
-			obj["Controversy Score"],
-		];
-		const sector = obj["Sector"];
-
+		const sector = obj["sector"];
 		res.json({
 			data: {
-				summary: summaries[ticker],
-				totalScore: T,
-				environmental: E,
-				governance: G,
-				social: S,
-				controversy: C,
-				sectorAverage: averageBySector[sector],
-				topCompanies: sectors[sector],
+				score: obj["total"],
+				environmental: obj["environmental"],
+				governance: obj["governance"],
+				social: obj["social"],
+				sector: sector,
+				sectorAverage: averageBySector[sector], // (E,S,G,T)
+				topCompanies: [], //sectors[sector]
+				summary: summaries[ticker] || "Unknown",
 			},
 		});
 	} else {
 		res.json({
-			data: null,
+			data: {},
 		});
 	}
 });
 
-app.get("/esg/news/:ticker", async (req, res) => {
-	let { ticker } = req.params;
-	ticker = ticker.toUpperCase();
+// app.get("/esg/summary/:ticker", async (req, res) => {
+// 	let { ticker } = req.params;
+// 	ticker = ticker.toUpperCase();
 
-	console.log(`Got summary request! Ticker: ${ticker}`);
-	const obj = tickers[ticker];
-	if (obj) {
-		const companyName = obj["Name"];
-		const summary = await esgSummary(companyName);
-		res.json({
-			data: { summary },
-		});
-	} else {
-		res.json({
-			data: null,
-		});
-	}
-});
+// 	console.log(`Got summary request! Ticker: ${ticker}`);
+// 	const obj = tickers[ticker];
+// 	if (obj) {
+// 		const companyName = obj["Name"];
+// 		const summary = await esgSummary(companyName);
+// 		res.json({
+// 			data: { summary },
+// 		});
+// 	} else {
+// 		res.json({
+// 			data: null,
+// 		});
+// 	}
+// });
 
-const PORT = 3000;
+// app.post("/esg/portfolio", async (req, res) => {
+// 	let { holdings } = req.body; // holdings = {ticker : float, ticker: float} float represents % of portfolio
+// 	if (!holdings) {
+// 		return res.json({ data: null });
+// 	}
+// 	let tscore,
+// 		escore,
+// 		sscore,
+// 		gscore,
+// 		percentage = 0;
+
+// 	for (let ticker in holdings) {
+// 		if (tickers[ticker] && tickers[tickers][total] != -1) {
+// 			escore += holdings[ticker] * tickers[ticker]["environmental"];
+// 			sscore += holdings[ticker] * tickers[ticker]["social"];
+// 			gscore += holdings[ticker] * tickers[ticker]["governance"];
+// 			tscore += holdings[ticker] * tickers[ticker]["total"];
+// 			percentage += holdings[ticker];
+// 		}
+// 	}
+
+// 	recip = percentage ? 1 / percentage : 1;
+// 	tscore,
+// 		escore,
+// 		sscore,
+// 		(gscore = tscore * recip),
+// 		escore * recip,
+// 		sscore * recip,
+// 		gscore * recip;
+
+// 	return res.json({
+// 		data: {
+// 			tscore,
+// 			escore,
+// 			sscore,
+// 			gscore,
+// 		},
+// 	});
+// });
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
