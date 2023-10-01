49ce73aac480924f734110b20d49bc112a876f20
KhromeM <khrome@nyu.edu>
2023-10-01 16:37:34 -0400

    portfolio scores

--- extension/portfolio.js
+++ extension/portfolio.js
@@ -21,51 +21,59 @@ function waitForElement(selector, timeout = 5000) {
 
 async function modifyElementContent() {
 	try {
-    const amountClass = "atrP1y1y_C9ULHV4BSwFj";
-    const tickerClass = "_2-4BkMtIykh6hAhu1CkOAi";
-    await waitForElement("." + amountClass);
+		const amountClass = "atrP1y1y_C9ULHV4BSwFj";
+		const tickerClass = "_2-4BkMtIykh6hAhu1CkOAi";
+		await waitForElement("." + amountClass);
 
-    const amounts = document.getElementsByClassName(amountClass);
-    const tickers = document.getElementsByClassName(tickerClass);
-    const holdings = {};
-    let total = 0;
+		const amounts = document.getElementsByClassName(amountClass);
+		const tickers = document.getElementsByClassName(tickerClass);
+		const holdings = {};
+		let total = 0;
 
-    for (let i = 1; i < amounts.length; i++) {
-      const amount = Number(amounts[i].textContent.replace(/[^0-9.-]+/g, ""));
-      const ticker = tickers[i].textContent;
-      holdings[ticker] = amount;
-      total += amount;
-    }
+		for (let i = 1; i < amounts.length; i++) {
+			const amount = Number(amounts[i].textContent.replace(/[^0-9.-]+/g, ""));
+			const ticker = tickers[i].textContent;
+			holdings[ticker] = amount;
+			total += amount;
+		}
 
-    for (const ticker of Object.keys(holdings)) {
-      holdings[ticker] /= total;
-    }
+		for (const ticker of Object.keys(holdings)) {
+			holdings[ticker] /= total;
+		}
 
-    const res = await getPortfolioScore(holdings);
-    const { tscore, escore, sscore, gscore } = res.data;
+		const res = await getPortfolioScore(holdings);
+		const { tscore, escore, sscore, gscore } = res.data;
 
-    // ADD DOM ELEMENTS TO SHOW SCORES
-    // Appending the ESG scores next to each ticker
-    for (let i = 0; i < tickers.length; i++) {
-      const tickerName = tickers[i].textContent.trim();
-      const esgData = res.data[tickerName];
+		// ADD DOM ELEMENTS TO SHOW SCORES
 
-      if (esgData) {
-        const esgDiv = document.createElement("div");
-        esgDiv.className = "esg-scores";
+		const esgDiv = document.createElement("div");
+		esgDiv.className = "portfolio-score";
 
-        const scores = ["T-score", "E-score", "S-score", "G-score"];
-        const scoreKeys = ["tscore", "escore", "sscore", "gscore"];
-        for (let j = 0; j < scores.length; j++) {
-          const scoreSpan = document.createElement("span");
-          scoreSpan.textContent = `${scores[j]}: ${esgData[scoreKeys[j]]}`;
-          esgDiv.appendChild(scoreSpan);
-        }
+		const t = document.createElement("span");
+		t.textContent = `ESG: ${tscore?.toFixed(2)}`;
+		const e = document.createElement("span");
+		e.textContent = `(E: ${escore?.toFixed(2)}`;
+		const s = document.createElement("span");
+		s.textContent = `S: ${sscore?.toFixed(2)}`;
+		const g = document.createElement("span");
+		g.textContent = `G: ${gscore?.toFixed(2)})`;
 
-        tickers[i].parentNode.appendChild(esgDiv);
-      }
-    }
-  } catch (error) {
+		t.style.marginRight = "10px";
+		for (const el of [e, s, g]) {
+			el.style.marginRight = "5px";
+			el.style.fontSize = ".7em";
+		}
+
+		esgDiv.appendChild(t);
+		esgDiv.appendChild(e);
+		esgDiv.appendChild(s);
+		esgDiv.appendChild(g);
+
+		const ele = [...document.querySelectorAll(".css-z4smye")].filter(
+			(el) => el.innerText == "Stocks"
+		)[0];
+		ele.insertBefore(esgDiv, ele.children[0]);
+	} catch (error) {
 		console.error("Error:", error.message);
 	}
 }
